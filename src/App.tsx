532fbbf5fd11c8d76f2368e4f56e5740a55b8da4
Vretamoza <valeryisabelretamoza@gmail.com>
2026-03-25 23:33:23 -0500

    feat: add daisyUI as component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>Hola mundo</>;
+  const [counter, setCounter] = useState(59);
+  return (
+    <>
+      <span className="countdown font-mono text-6xl">
+        <span
+          style={{ "--value": counter, "--digits": 2 } as React.CSSProperties}
+          aria-live="polite"
+          aria-label={counter.toString()}
+        >
+          {counter}
+        </span>
+      </span>
+      <button onClick={() => setCounter(counter - 1)}>Decrementar</button>
+    </>
+  );
 }
 
 export default App;
